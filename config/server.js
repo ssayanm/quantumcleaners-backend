d68ca66a5a7fffdd6367f3c49d431cf4fa786f17
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-09-21 08:32:29 +0530

    added slider and updated port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1338),
+  port: env.int("PORT", 1336),
   // url: "https://admin.duecoaching.com/",
   admin: {
     auth: {
